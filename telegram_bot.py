8ea59d03836ba8d3c2d5ddaf8d1e71f78bbfe64d
Skiipz <corne.nick@gmail.com>
2022-06-27 15:56:34 +0200

    Add additional checks to ensure the API token is found and correct

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,7 +1,28 @@
-import telebot
+import sys, configparser, telebot
 
-bot = telebot.TeleBot("xxxxxxxxxxxxxxxxx") 
+config = configparser.ConfigParser()
 
+# Open settings.ini file to retrieve API token
+try:
+	with open('settings.ini') as f:
+		config.read_file(f)
+except:
+	e = sys.exc_info()[1]
+	print(e)
+	sys.exit(1)
+
+token = config.get('TelegramSettings','API-Token')
+
+# initialise Bot 
+try:
+	bot = telebot.TeleBot(token) 
+	bot_info = bot.get_me()
+except:
+	e = sys.exc_info()[1]
+	print(e)
+	sys.exit(1)
+
+# Bot handlers below
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
 	bot.reply_to(message, "Howdy, how are you doing?")
@@ -10,4 +31,5 @@ def send_welcome(message):
 def echo_all(message):
 	bot.reply_to(message, message.text)
 
+# Start the bot
 bot.infinity_polling()
