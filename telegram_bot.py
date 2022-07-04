ddbca72ef58324cbf0aa1a064e714230f958dda0
Sven Putteneers <sputtene@gmail.com>
2022-07-04 17:03:28 +0200

    Add config option precedence logic
    Both the command line and the config file options are parsed.
    Options from the command line have precedence over the same options from
    the config file.
    
    It should not be that difficult to add options from environment
    variables to the mix as well (see the loop on lines 79-83 for the actual
    priorization logic) when we want to allow configuration through those as
    well.
    Since containers are (usually) configured through environment variables,
    that might be an interesting use case to add that.

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,14 +2,14 @@
 """
 Usage: %(script_name)s { -h | --help }
        %(script_name)s [ { -c | --config-file } FILE ]
-            [ { -i | --telegram-id } UUID }
+            [ { -t | --telegram-id } UUID }
 
 Start the Telegram/Zabbix bot.
 
 Arguments:
     -h, --help      This message
     -c FILE, --config-file FILE     The location of the config file.
-    -i UUID, --telegram-id UUID     The UUID of your Telegram bot as returned
+    -t UUID, --telegram-id UUID     The UUID of your Telegram bot as returned
                     by @BotFather. It is discouraged to specify this on the
                     command line (although it is useful for testing); use
                     the config file instead.
@@ -29,10 +29,12 @@ def usage():
 
 
 def parse_commandline(argv = sys.argv[1:]):
+    cmdline_config = {}
+
     try:
         optlist, args = getopt.getopt(
                 argv,
-                'c:hi:',
+                'c:ht:',
                 [ 'config-file=', 'help', 'telegram-id=' ]
         )
     except getopt.GetoptError as err:
@@ -42,28 +44,50 @@ def parse_commandline(argv = sys.argv[1:]):
         sys.exit(1)
 
     for opt, arg in optlist:
-        # TODO Parse command line options
-        pass
+        if opt in ('-h', '--help'):
+            usage()
+            sys.exit(0)
+        elif opt in ('-c', '--config-file'):
+            cmdline_config['config-file'] = arg
+        elif opt in ('-t', '--telegram-id'):
+            cmdline_config['telegram-id'] = arg
+        else:
+            assert False, 'Unhandled command line option [%(opt)s]' % {'opt': opt};
 
+    return cmdline_config
 
 
 def main():
     logging.basicConfig(format='%(message)s')
 
-    parse_commandline()
+    cmdline_config = parse_commandline()
 
-    config = configparser.ConfigParser()
-
+    configfile_parser = configparser.ConfigParser()
     # Open settings.ini file to retrieve API token
     try:
-            with open('settings.ini') as f:
-                    config.read_file(f)
+            with open(cmdline_config['config-file'] if 'config-file' in cmdline_config else 'settings.ini') as f:
+                    configfile_parser.read_file(f)
     except:
             e = sys.exc_info()[1]
             print(e)
             sys.exit(1)
 
-    token = config.get('TelegramSettings','API-Token')
+    # Put configuration in the actual config dictionary. Use the value specified
+    # on the command line if it has a value, fall back to the config file if it
+    # does not.
+    config = {}
+    for cmdline_option, configfile_option, name in [
+        ( 'telegram-id', ('TelegramSettings', 'API-Token'), 'telegram-API-token' ),
+    ]:
+        print("Parsing config option %(name)s" % {'name': name})
+        config[name] = cmdline_config[cmdline_option] if cmdline_option in cmdline_config else configfile_parser.get(configfile_option[0], configfile_option[1], fallback=None)
+
+    token = config['telegram-API-token']
+
+    if token == '':
+        log = logging.getLogger(__name__)
+        log.error('No Telegram API token specified. Configure it in the config file or specify it on the command line')
+        sys.exit(1)
 
     # initialise Bot
     try:
